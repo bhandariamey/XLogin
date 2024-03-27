3ed6fd6b1f7a2d4d7ad2c5816bede3fbfb263da3
bhandariamey <bhandariamey@gmail.com>
2024-03-27 15:18:09 +0530

    d

--- src/App.js
+++ src/App.js
@@ -1,51 +1,65 @@
-import { useState } from 'react';
-import styles from './App.module.css'
+import React, { useState } from 'react';
+import styles from './App.module.css';
 
 function App() {
-
-  
-  const [details, setDetails] = useState(false)
-  const [incorrect, setIncorrect] = useState(false)
-  const [userName, setUserName] = useState('')
-  const [password, setPassword] = useState('')
+  const [details, setDetails] = useState(false);
+  const [incorrect, setIncorrect] = useState(false);
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleFormSubmit = (e) => {
-
-    e.preventDefault()
-    if(userName === 'user' && password === 'password'){
-      setDetails(true)
-      setIncorrect(false); 
+    e.preventDefault();
+    if (userName === 'user' && password === 'password') {
+      setDetails(true);
+      setIncorrect(false);
+    } else {
+      setIncorrect(true);
     }
-    
-    else{
-      setIncorrect(true)
-    }
-
-  }
+  };
 
   return (
     <>
-    <h1>Login Page</h1>
+      <h1>Login Page</h1>
 
-    {incorrect && <p>Invalid username or password</p>}
+      {incorrect && <p>Invalid username or password</p>}
 
-    { !details && <form onSubmit={handleFormSubmit} className={styles.form}>
-      
-      <label>Username
-      <input onChange={(e)=>{setUserName(e.target.value)}} type='text' name='username' required value={userName}/>
-      </label>
+      {!details && (
+        <form onSubmit={handleFormSubmit} className={styles.form}>
+          <label>
+            Username
+            <input
+              onChange={(e) => {
+                setUserName(e.target.value);
+              }}
+              type="text"
+              name="username"
+              required
+              value={userName}
+            />
+          </label>
 
-      <label>Password
-      <input onChange={(e)=>{setPassword(e.target.value)}} type='password' name= 'password' required value={password}/>
-      </label>
+          <label>
+            Password
+            <input
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              type="password"
+              name="password"
+              required
+              value={password}
+            />
+          </label>
 
-      <button style={{width:'max-content'}} type="submit" name='login'>Submit</button>
-    </form>}
+          <button style={{ width: 'max-content' }} type="submit" name="login">
+            Submit
+          </button>
+        </form>
+      )}
 
-    {details && <p>Welcome, {userName}</p>}
-
+      {details && <p>Welcome, {userName}</p>}
     </>
-    );
+  );
 }
 
 export default App;
