43574fbb8e87c203f2e98e775d33a397d54f09a4
bhandariamey <bhandariamey@gmail.com>
2024-03-29 12:54:38 +0530

    c

--- src/App.js
+++ src/App.js
@@ -1,70 +1,58 @@
-import React, { useState } from 'react';
-import styles from './App.module.css';
+import React, { useState } from 'react'
+import styles from "./style.css"
 
-function App() {
-  const [details, setDetails] = useState(false);
-  const [incorrect, setIncorrect] = useState(false);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+// import Child from './Child';
+export default function LoginPage() {
+    let [ip,setIp]=useState({user:"",pwd:""})
+    let [f,setF]=useState(true)
 
-  const handleFormSubmit = (e) => {
+    let [errorMsg,setErrorMsg]=useState(false);
+    let [welcomeFlag,setwelcomeFlag]=useState(false)
+
+
+    function handleSubmit(e)
+    {
     e.preventDefault();
-    if (username === 'user' && password === 'password') {
-      setDetails(true);
-      setIncorrect(false);
-    } else {
-      setIncorrect(true);
-      setDetails(false); // Reset details state on incorrect login attempt
+    if(ip.user==="user" && ip.pwd==="password")
+    {
+    setF(false)
+    setwelcomeFlag(true)
+    setErrorMsg(false)
     }
-  };
+    else{
+        setErrorMsg("Invalid username or password")
+    }
+
+    }
+    function handleChange(e){
+        setIp((prev)=>{
+            return {...prev,[e.target.name]:e.target.value}
+        })
+        
+    }
 
   return (
-    <>
-      <h1>Login Page</h1>
+    <div>
+    {errorMsg &&<h5 style={{color:"red"}}>{errorMsg}</h5>}  
+    <h1>Login Page</h1>
+    {f&&<form className={styles.container} onSubmit={handleSubmit}>
+    
+    <div className={styles.inputContainer}>
+        <label htmlFor='user'> Username:</label>
+        <input type="text" required name="user" id="user" onChange={handleChange} value={ip.fname} placeholder='username'/>
 
-      {incorrect && <p>Invalid username or password</p>}
+    </div>
+    <div className={styles.inputContainer}>
+        <label htmlFor='pwd'> Password:</label>
+        <input type="password" required name="pwd" id="pwd" onChange={handleChange} value={ip.lname} placeholder='password'/>
 
-      {!details && (
-        <form onSubmit={handleFormSubmit} className={styles.form}>
-          <label htmlFor='Username'>
-            Username
-            <input
-              onChange={(e) => {
-                setUsername(e.target.value);
-                setIncorrect(false); // Clear incorrect message when user starts typing
-              }}
-              id='Username'
-              type="text"
-              name="Username"
-              required
-              value={username}
-            />
-          </label>
+    </div>
+    <div className={styles.buttonContainer}>
+        <button type="submit" >Submit</button>
+    </div> 
+    </form>}
 
-          <label htmlFor='Password'>
-            Password
-            <input
-              onChange={(e) => {
-                setPassword(e.target.value);
-                setIncorrect(false); // Clear incorrect message when user starts typing
-              }}
-              id='Password'
-              type="password"
-              name="Password"
-              required
-              value={password}
-            />
-          </label>
-
-          <button style={{ width: 'max-content' }} type="submit" name="login">
-            Submit
-          </button>
-        </form>
-      )}
-
-      {details && <p>Welcome, {username}</p>}
-    </>
-  );
+   {welcomeFlag&& <p style={{margin:"1rem"}}>Welcome, {ip.user}!</p>}        
+    </div>
+  )
 }
-
-export default App;
